3c3475174e14276c8f90193082637e648059bb8c
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-07-20 20:25:10 -0600

    Minor changes

--- yinsh/game.py
+++ yinsh/game.py
@@ -8,9 +8,7 @@ from yinsh.types import Hex, IllegalMoveError, Player, Players
 class Move:
     def __init__(self, src_hex: Hex, dst_hex: Hex = None, is_starting: bool = False):
         # Replace with proper exception
-        assert (
-            dst_hex is not None
-        ) ^ is_starting, "Move takes exactly 1 type of move as input"
+        assert (dst_hex is not None) ^ is_starting, "Move takes exactly 1 type of move as input"
 
         self.src_hex = src_hex
         self.dst_hex = dst_hex
@@ -22,9 +20,7 @@ class Move:
     def play(cls, src_hex: Hex, dst_hex: Hex):
         # issubset(other)
         if not {src_hex, dst_hex} <= valid_hexes:
-            raise IllegalMoveError(
-                f"Hexes must be valid board locations: {src_hex}, {dst_hex}"
-            )
+            raise IllegalMoveError(f"Hexes must be valid board locations: {src_hex}, {dst_hex}")
         return Move(src_hex, dst_hex)
 
     @classmethod
@@ -32,6 +28,12 @@ class Move:
         if hex not in valid_hexes:
             raise IllegalMoveError(f"Hex must be valid board location: {hex}")
         return Move(hex, is_starting=True)
+
+    def __repr__(self):
+        if self.is_play:
+            return f"Move.play({self.src_hex}, {self.dst_hex})"
+        else:
+            return f"Move.place({self.src_hex})"
 
 
 class GameState:
@@ -52,8 +54,8 @@ class GameState:
         self.last_move = move
         self.variant = variant
 
+        self.requires_setup = not is_setup
         self._rings_to_win = 1 if self.variant == "blitz" else 3
-        self._requires_setup = not is_setup
 
     @classmethod
     def new_game(cls, variant: str = "standard"):
@@ -70,24 +72,36 @@ class GameState:
         ], "Variant must be 'standard', 'blitz', or not given"
 
         players = Players()
+        players.white.set_rings(0)
+        players.black.set_rings(0)
 
-        return GameState(
-            Board.empty(), players, players.white, None, None, variant, is_setup=False
-        )
+        return GameState(Board.empty(), players, players.white, None, None, variant, is_setup=False)
 
-    def is_over(self):
+    def is_over(self, outcome: bool = False):
+        # Return Outcome object if True
         return (
             self.players.white.rings == self._rings_to_win
             or self.players.black.rings == self._rings_to_win
         )
 
     def make_move(self, move: Move):
-        if move.is_play and self._requires_setup:
+        if move.is_play and self.requires_setup:
             raise IllegalMoveError("Board requires setup")
-        if self.is_over():
+        elif move.is_starting and not self.requires_setup:
+            raise IllegalMoveError("Max rings reached")
+        elif self.is_over():
             raise IllegalMoveError("Game is over")
 
         if move.is_starting:
-            ...
+            raise NotImplementedError
         elif move.is_play:
-            ...
+            raise NotImplementedError
+
+    def legal_moves(self):
+        if self.requires_setup:
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+    def display(self):
+        print(self.board._grid)
