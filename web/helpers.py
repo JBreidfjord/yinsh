5131bb6388283478f501d3f89f7accbcc0a6e59b
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-08-25 11:09:30 -0600

    Route and helpers to handle place move

---
+++ web/helpers.py
@@ -0,0 +1,44 @@
+import json
+
+from yinsh.game import GameState, Move
+from yinsh.helpers import coordinate_index
+from yinsh.types import Hex, IllegalMoveError, Marker, Ring
+
+content_index = {None: 0, Ring.WHITE: 1, Ring.BLACK: 2, Marker.WHITE: 3, Marker.BLACK: 4}
+inv_content_index = {v: k for k, v in content_index.items()}
+
+
+def handle_place(hex: Hex, game: GameState):
+    if game.is_over() or not game.requires_setup:
+        return
+    move = Move.place(hex)
+    try:
+        game.make_move(move)
+        return dump_data(game)
+    except IllegalMoveError:
+        return
+
+
+def handle_play(hex: Hex, game: GameState):
+    ...
+
+
+def parse_data(data: dict):
+    print(data)
+    hex = Hex(*data["action"].values())
+    game = GameState.parse_state(data["state"])
+    return hex, game
+
+
+def dump_data(game: GameState):
+    grid = {
+        coordinate_index[hex]: content_index[content] for hex, content in game.board._grid.items()
+    }
+    data = {
+        "state": {
+            "grid": grid,
+            "rings": {"white": game.players.white.rings, "black": game.players.black.rings},
+            "requiresSetup": game.requires_setup,
+        }
+    }
+    return json.dumps(data)
--- web/main.py
+++ web/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from web.helpers import handle_place, handle_play, parse_data
 
 app = FastAPI()
 
@@ -8,6 +11,25 @@ app.mount("/static", StaticFiles(directory="web/static"), name="static")
 templates = Jinja2Templates(directory="web/templates")
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/random", response_class=JSONResponse)
+async def random(request: Request, state):
+    ...
+
+
+@app.post("/place", response_class=JSONResponse)
+async def place(request: Request):
+    data = await request.json()
+    response_data = handle_place(*parse_data(data))
+    if response_data is None:
+        raise HTTPException(409, "Action not valid for given state")
+    return JSONResponse(response_data)
+
+
+@app.post("/play", response_class=JSONResponse)
+async def play(request: Request):
+    data = await request.json()
